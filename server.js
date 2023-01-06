b833098662f7f129592b3b9dcc34eafb1f0def3f
[Sarah Lang] <[lynnlang98@gmail.com]>
2023-01-05 20:42:52 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3001;
+
+// Middleware for parsing json
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// GET route for notes page
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './Develope/db', 'db.json')));
+
+// GET route for homepage
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develope/public', 'notes.html')));
+
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync('./Develop/db.json', 'utf8'));
+    let notelength = (noteList.length).toString();
+
+    newNote.id = notelength;
+    noteList.push(newNote);
+    
+    fs.writeFileSync('./Develop/db.json', JSON.stringify(noteList));
+    res.json(noteList);
+})
+
+app.listen(PORT);
