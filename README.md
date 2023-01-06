379f2c9a78b232a58fdea1f0542fb2edd43767d0
[Sarah Lang] <[lynnlang98@gmail.com]>
2023-01-06 11:45:25 -0500

    DELETE /api/notes/:id working

--- README.md
+++ README.md
@@ -25,6 +25,9 @@ THEN I am presented with empty fields to enter a new note title and the note’s
 ## GitHub Repository
 https://github.com/sarahlang9800/note-taker
 
+## Link to Deployed Application
+https://sarah-lang-note-taker.herokuapp.com/
+
 ## Example Photo 
 ![Note Taker Example Picture](./img/Note-Taker-Example-Picture.png)
 
--- server.js
+++ server.js
@@ -11,27 +11,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // GET route for notes page
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './db', 'db.json')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public', 'notes.html')));
 
 // GET route for homepage
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public', 'notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public', 'index.html')));
 
-app.get('./api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
-        res.json(JSON.parse(data))
-    })
-})
+        res.json(JSON.parse(data));
+    });
+});
 
+// Post notes 
 app.post('/api/notes', (req, res) => {
     let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     let notelength = (noteList.length).toString();
 
     newNote.id = notelength;
     noteList.push(newNote);
 
-    fs.writeFileSync('./db.json', JSON.stringify(noteList));
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
     res.json(noteList);
-})
+});
 
+// Deletes note by selected ID 
+app.delete('/api/notes/:id', (request, result) => {
+    let noteName = request.params.id;
+    
+    fs.readFile('./db/db.json', 'utf-8', (err, response) => {
+        if (err) throw (err);
+        const everyNote = JSON.parse(response);
+        const newNotes = everyNote.filter(note => note.id != noteName);
+        fs.writeFile('./db/db.json', JSON.stringify(newNotes, null, 2),
+            err => {
+                if (err) throw err;
+                result.json(true);
+            }
+        );
+      });
+    });
+    
 app.listen(PORT);
